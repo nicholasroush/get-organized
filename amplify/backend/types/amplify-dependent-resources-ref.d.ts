efc2e49b41fa1ca6ed4519a84b8a616b0b1e94cc
ImCalicoJack <roush757@gmail.com>
2023-06-20 13:12:54 -0400

    auth added

---
+++ amplify/backend/types/amplify-dependent-resources-ref.d.ts
@@ -0,0 +1,13 @@
+export type AmplifyDependentResourcesAttributes = {
+  "auth": {
+    "getorganizedd5502b0d": {
+      "AppClientID": "string",
+      "AppClientIDWeb": "string",
+      "IdentityPoolId": "string",
+      "IdentityPoolName": "string",
+      "UserPoolArn": "string",
+      "UserPoolId": "string",
+      "UserPoolName": "string"
+    }
+  }
+}
--- src/App.js
+++ src/App.js
@@ -1,11 +1,23 @@
 import "./App.css";
+import "@aws-amplify/ui-react/styles.css";
+import {
+	withAuthenticator,
+	Button,
+	Heading,
+	Image,
+	View,
+	Card,
+} from "@aws-amplify/ui-react";
 
-function App() {
+function App({ signOut }) {
 	return (
-		<div className='App'>
-			<h1>TEST</h1>
-		</div>
+		<View className='App'>
+			<Card>
+				<Heading level={1}>Temp Auth Heading</Heading>
+			</Card>
+			<Button onClick={signOut}>Sign Out</Button>
+		</View>
 	);
 }
 
-export default App;
+export default withAuthenticator(App);
