4ac9262c71f67981bec7648b6e4f31a940c87ed0
ImCalicoJack <roush757@gmail.com>
2023-06-21 17:26:49 -0400

    updates to styling and table layout

--- src/App.js
+++ src/App.js
@@ -6,7 +6,6 @@ import {
 	Button,
 	Flex,
 	Heading,
-	Text,
 	View,
 	withAuthenticator,
 } from "@aws-amplify/ui-react";
@@ -14,32 +13,50 @@ import { listTodos } from "./graphql/queries";
 import {
 	createTodo as createTodoMutation,
 	deleteTodo as deleteTodoMutation,
+	updateTodo as updateTodoMutation,
 } from "./graphql/mutations";
-import { Input, Select, DatePicker } from "antd";
+import { Input, Select, DatePicker, Table, Dropdown } from "antd";
+import ellipsis from "./imgs/ellipsis.png";
+import EditorModal from "./EditorModal/EditorModal";
 
 const { TextArea } = Input;
 
 const selectOptions = [
 	{
 		label: "Not Started",
-		value: "not started",
+		value: "Not Started",
 	},
 	{
 		label: "In Progress",
-		value: "in progress",
+		value: "In Progress",
 	},
 	{
 		label: "Completed",
-		value: "completed",
+		value: "Completed",
 	},
 ];
 
-const App = ({ signOut }) => {
+const menuItems = [
+	{
+		key: "1",
+		label: "Edit",
+	},
+	{
+		key: "2",
+		label: "Delete",
+	},
+];
+
+const App = ({ signOut, user }) => {
 	const [notes, setNotes] = useState([]);
 	const [title, setTitle] = useState("");
 	const [description, setDescription] = useState("");
 	const [status, setStatus] = useState("Not Started");
 	const [date, setDate] = useState();
+	const [openEditor, setOpenEditor] = useState(false);
+	const [editorId, setEditorId] = useState();
+	const activeUser = user.username;
+	const username = activeUser.charAt(0).toUpperCase() + activeUser.slice(1);
 
 	useEffect(() => {
 		fetchNotes();
@@ -64,7 +81,47 @@ const App = ({ signOut }) => {
 			authMode: GRAPHQL_AUTH_MODE.AMAZON_COGNITO_USER_POOLS,
 		});
 		fetchNotes();
+		setTitle("");
+		setDescription("");
+		setStatus("Not Started");
+		setDate();
 	}
+
+	async function updateNote() {
+		const data = {
+			id: editorId,
+			title: title,
+			description: description,
+			status: status,
+			dueDate: date,
+		};
+
+		await API.graphql({
+			query: updateTodoMutation,
+			variables: { input: data },
+		});
+
+		fetchNotes();
+		setTitle("");
+		setDescription("");
+		setStatus("Not Started");
+		setDate();
+		setOpenEditor(!openEditor);
+	}
+
+	const editNote = ({ id }) => {
+		setOpenEditor(!openEditor);
+		setEditorId(id);
+		notes.map((el) => {
+			if (el.id === id) {
+				setTitle(el.title);
+				setDescription(el.description);
+				setStatus(el.status);
+				setDate(el.date);
+			}
+			return el;
+		});
+	};
 
 	async function deleteNote({ id }) {
 		const newNotes = notes.filter((note) => note.id !== id);
@@ -75,61 +132,183 @@ const App = ({ signOut }) => {
 		});
 	}
 
+	const handleMenuClick = (e, id) => {
+		switch (e.key) {
+			case "1":
+				editNote(id);
+				break;
+
+			case "2":
+				deleteNote(id);
+				break;
+
+			default:
+				break;
+		}
+	};
+
+	const tableColumns = [
+		{
+			title: "Title",
+			dataIndex: "title",
+			key: "name",
+			sorter: {
+				compare: (a, b) => a.title.localeCompare(b.title),
+			},
+		},
+		{
+			title: "Description",
+			dataIndex: "description",
+			key: "description",
+			sorter: {
+				compare: (a, b) => a.description.localeCompare(b.description),
+			},
+			width: "50%",
+		},
+		{
+			title: "Status",
+			dataIndex: "status",
+			key: "status",
+			sorter: {
+				compare: (a, b) => a.status.localeCompare(b.status),
+			},
+		},
+		{
+			title: "Due Date",
+			dataIndex: "dueDate",
+			key: "dueDate",
+			sorter: {
+				compare: (a, b) => a.dueDate.localeCompare(b.dueDate),
+			},
+		},
+		{
+			title: "Actions",
+			dataIndex: "actions",
+			key: "actions",
+			render: (_, id) => {
+				return (
+					<Dropdown
+						trigger={["click"]}
+						menu={{
+							items: menuItems,
+							onClick: (e) => handleMenuClick(e, id),
+						}}
+					>
+						<img
+							src={ellipsis}
+							alt='More Options'
+							style={{ height: "1.25rem", width: "auto", cursor: "pointer" }}
+						/>
+					</Dropdown>
+				);
+			},
+			align: "center",
+		},
+	];
+
 	return (
 		<View className='App'>
-			<Heading level={1}>My Notes App</Heading>
+			{openEditor && (
+				<EditorModal
+					updateNote={updateNote}
+					title={title}
+					setTitle={setTitle}
+					description={description}
+					setDescription={setDescription}
+					status={status}
+					setStatus={setStatus}
+					setDate={setDate}
+					setOpenEditor={setOpenEditor}
+					openEditor={openEditor}
+				/>
+			)}
+			<Heading level={1} style={{ margin: "3rem 0" }}>
+				{username}'s Organizer App
+			</Heading>
 			<Flex direction='Row' justifyContent='center'>
 				<View>
 					<Input
+						name='title'
 						placeholder='Title'
 						value={title}
 						onChange={(e) => setTitle(e.target.value)}
+						style={{ margin: "1rem", width: "50%" }}
 					/>
 					<TextArea
 						placeholder='A short description.'
 						allowClear
 						value={description}
 						onChange={(e) => setDescription(e.target.value)}
+						style={{ margin: "1rem" }}
 					>
 						<br />
 						<br />
 					</TextArea>
-					<Select
-						options={selectOptions}
-						value={status}
-						onChange={(value) => setStatus(value)}
-						style={{ width: "auto" }}
-					/>
-					<DatePicker
-						allowClear
-						onChange={(date) => setDate(date.format("MM-DD-YYYY"))}
-					/>
+					<div
+						style={{
+							display: "flex",
+							justifyContent: "center",
+							alignItems: "center",
+						}}
+					>
+						<div
+							style={{
+								display: "flex",
+								flexDirection: "column",
+								width: "fit-content",
+							}}
+						>
+							<label htmlFor='status'>Status</label>
+							<Select
+								name='status'
+								options={selectOptions}
+								value={status}
+								onChange={(value) => setStatus(value)}
+								style={{ width: "auto", margin: "0 1rem", padding: ".5rem" }}
+							/>
+						</div>
+						<div
+							style={{
+								display: "flex",
+								flexDirection: "column",
+								width: "fit-content",
+							}}
+						>
+							<label htmlFor='dueDate'>Due Date</label>
+							<DatePicker
+								name='dueDate'
+								allowClear
+								onChange={(date) => setDate(date.format("YYYY-MM-DD"))}
+								style={{ margin: ".5rem 1rem" }}
+							/>
+						</div>
+					</div>
 					<br />
-					<Button onClick={createTodo}>Sumbit</Button>
+					<Button onClick={createTodo} style={{ margin: "1rem" }}>
+						Sumbit
+					</Button>
 				</View>
 			</Flex>
 			<View margin='3rem 0'>
-				{notes.map((note) => (
-					<Flex
-						key={note.id || note.title}
-						direction='column'
-						justifyContent='center'
-						alignItems='center'
-					>
+				{notes.length > 0 && (
+					<Flex direction='column' justifyContent='center' alignItems='center'>
 						<Heading level={2}>Current Notes</Heading>
-						<Text as='strong' fontWeight={700}>
-							{note.title}
-						</Text>
-						<Text as='span'>{note.description}</Text>
-						<Text as='span'>{note.status}</Text>
-						<Text as='span'>{note.dueDate}</Text>
-						<Button variation='link' onClick={() => deleteNote(note)}>
-							Delete note
-						</Button>
+						<Table
+							rowKey='id'
+							bordered
+							dataSource={notes}
+							columns={tableColumns}
+							style={{ width: "60%" }}
+						/>
 					</Flex>
-				))}
+				)}
 			</View>
-			<Button onClick={signOut}>Sign Out</Button>
+			<Button
+				onClick={signOut}
+				style={{ position: "absolute", top: "1rem", right: "1rem" }}
+			>
+				Sign Out
+			</Button>
 		</View>
 	);
 };
---
+++ src/EditorModal/EditorModal.js
@@ -0,0 +1,143 @@
+import React from "react";
+import { Button } from "@aws-amplify/ui-react";
+import { Input, Select, DatePicker } from "antd";
+import close from "../imgs/close.png";
+import "./editorModal.css";
+
+const { TextArea } = Input;
+
+const selectOptions = [
+	{
+		label: "Not Started",
+		value: "Not Started",
+	},
+	{
+		label: "In Progress",
+		value: "In Progress",
+	},
+	{
+		label: "Completed",
+		value: "Completed",
+	},
+];
+
+const EditorModal = ({
+	updateNote,
+	title,
+	setTitle,
+	description,
+	setDescription,
+	status,
+	setStatus,
+	setDate,
+	setOpenEditor,
+	openEditor,
+}) => {
+	const closeModal = () => {
+		setTitle("");
+		setDescription("");
+		setStatus("Not Started");
+		setDate();
+		setOpenEditor(!openEditor);
+	};
+	return (
+		<div className='modal-container'>
+			<div className='modal-mask' />
+			<div className='modal-content'>
+				<img src={close} alt='CLOSE' onClick={closeModal} />
+				<div
+					style={{
+						display: "flex",
+						flexDirection: "column",
+						alignItems: "flex-start",
+						width: "50%",
+						margin: "auto",
+					}}
+				>
+					<label htmlFor='title' style={{ fontWeight: "500" }}>
+						Title
+					</label>
+					<Input
+						name='title'
+						placeholder='Title'
+						value={title}
+						onChange={(e) => setTitle(e.target.value)}
+						style={{ margin: "1rem", width: "100%", alignSelf: "center" }}
+					/>
+				</div>
+				<div
+					style={{
+						display: "flex",
+						flexDirection: "column",
+						alignItems: "flex-start",
+						width: "100%",
+						margin: "auto",
+					}}
+				>
+					<label htmlFor='title' style={{ fontWeight: "500" }}>
+						Description
+					</label>
+					<TextArea
+						placeholder='A short description.'
+						allowClear
+						value={description}
+						onChange={(e) => setDescription(e.target.value)}
+						style={{ margin: "1rem", alignSelf: "center" }}
+					>
+						<br />
+						<br />
+					</TextArea>
+				</div>
+				<div
+					style={{
+						display: "flex",
+						justifyContent: "center",
+						alignItems: "center",
+					}}
+				>
+					<div
+						style={{
+							display: "flex",
+							flexDirection: "column",
+							width: "fit-content",
+						}}
+					>
+						<label htmlFor='status' style={{ fontWeight: "500" }}>
+							Status
+						</label>
+						<Select
+							name='status'
+							options={selectOptions}
+							value={status}
+							onChange={(value) => setStatus(value)}
+							style={{ width: "auto", margin: "0 1rem", padding: ".5rem" }}
+						/>
+					</div>
+					<div
+						style={{
+							display: "flex",
+							flexDirection: "column",
+							width: "fit-content",
+						}}
+					>
+						<label htmlFor='dueDate' style={{ fontWeight: "500" }}>
+							Due Date
+						</label>
+						<DatePicker
+							name='dueDate'
+							allowClear
+							onChange={(date) => setDate(date.format("YYYY-MM-DD"))}
+							style={{ margin: ".5rem 1rem" }}
+						/>
+					</div>
+				</div>
+				<br />
+				<Button onClick={updateNote} style={{ margin: "1rem" }}>
+					Update
+				</Button>
+			</div>
+		</div>
+	);
+};
+
+export default EditorModal;
